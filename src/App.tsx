c5814ea22771f46c0575e08f3c4ec4359ec65061
Daniel Damasio <damasio.dan7@gmail.com>
2023-02-25 23:47:13 -0300

    added portifolio section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import BannerComponent from './components/Banner'
 import NavComponent from './components/Navbar'
+import PortifolioItemComponent from './components/PortifolioItem'
 
 function App() {
   return (
@@ -21,6 +20,27 @@ function App() {
             <p>Lorem Ipsum is simply dummy text of the printing and typeseLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'sum has been the industry's</p>
           </article>
         </div>
+      </section>
+
+      <section className='portifolio'>
+        <article className='portifolio__wrapper container'>
+          <h1 className='portifolio__title'>Portifólio</h1>
+          <PortifolioItemComponent data={{ 
+            title: 'Project 1', 
+            description: 'This project consists of an application for tracking orders. Just enter the tracking code, and if valid, the information is returned in detail. The data comes through an API. Developed with React.js.',
+            sorce_code: 'https://github.com/damasioCode',
+            live_preview: 'https://github.com/damasioCode',
+            image: 'https://png.pngtree.com/thumb_back/fw800/background/20211223/pngtree-minimalism-minimalist-landscape-artist-artwork-digital-art-hd-bear-wallpaper-image_934122.jpg',
+          }} />
+          <PortifolioItemComponent data={{ 
+            title: 'Project 1', 
+            description: 'This project consists of an application for tracking orders. Just enter the tracking code, and if valid, the information is returned in detail. The data comes through an API. Developed with React.js.',
+            sorce_code: 'https://github.com/damasioCode',
+            live_preview: '',
+            image: 'https://png.pngtree.com/thumb_back/fw800/background/20211223/pngtree-minimalism-minimalist-landscape-artist-artwork-digital-art-hd-bear-wallpaper-image_934122.jpg',
+          }} />
+
+        </article>
       </section>
     </div>
   )
---
+++ src/components/PortifolioItem/index.tsx
@@ -0,0 +1,43 @@
+import './index.scss'
+
+interface PortifolioItemProps {
+    data: {
+        title: string;
+        description: string;
+        sorce_code: string;
+        live_preview?: string;
+        image: string;
+    }
+}
+
+const PortifolioItemComponent = ({ 
+    data: {
+        title ,
+        description, 
+        sorce_code, 
+        live_preview, 
+        image
+    } } : PortifolioItemProps) => {
+        
+    return (
+        <article className='portifolio__item'>
+            <div className='content-1 container'>
+                <h2 className='portifolio__item--title'>{title}</h2>
+                <p className='portifolio__item--description'>{description}</p>
+                <div className='portifolio__item--link'>
+                    <a href={sorce_code} target='_blank' className='portifolio__item--source'>Source code</a>
+                    {live_preview ? ( 
+                        <a href={live_preview} target='_blank' className='portifolio__item--preview'>Live Preview</a> 
+                    ) : ''}
+                </div>
+            </div>
+            <div className='content-2 container'>
+                <div className='portifolio__item--imagem'>
+                    <img src={image} alt={`${title} imagem`} />
+                </div>
+            </div>
+        </article>
+    )
+}
+
+export default PortifolioItemComponent 
