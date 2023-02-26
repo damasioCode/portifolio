bfdaa77362b88af4495eef2af29f738f83e20faa
Daniel Damasio <damasio.dan7@gmail.com>
2023-02-26 01:34:45 -0300

    remove emoji animations

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,11 +4,11 @@ const BannerComponent = () => {
     return (
         <section className='banner'>
             <div className='banner__block container'>
-                <p>Eai! <span className='hangloose'>🤙</span></p>
+                <p>Eai! 🤙</p>
                 <h1 className='spacing'>Eu sou Daniel Damasio.</h1>
                 <h1>Desenvolvo coisas pela web.</h1>
                 <p className='spacing'>Desenvolvedor Backend focado na criatividade.</p>
-                <p>Eu <span className='heart'>❤️</span>o que faço, valoriso boas práticas e código limpo.</p>
+                <p>Eu ❤️ o que faço, valoriso boas práticas e código limpo.</p>
                 <a href="#contact" className='button__primary spacing'>Entre em contato</a>
             </div>
         </section>
